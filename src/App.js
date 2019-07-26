a5e2cc4add68a13d9a7c1f025bd815dd0810e5a1
Ross Mawdsley <rossmawd@gmail.com>
2019-07-26 13:33:20 +0100

    heroku test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          TEST of the Heroku push to master command
         </p>
         <a
           className="App-link"
