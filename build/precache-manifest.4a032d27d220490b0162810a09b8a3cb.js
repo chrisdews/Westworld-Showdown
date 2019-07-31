8f879e44a27950621c80190d90f2b9b892158e32
Ross Mawdsley <rossmawd@gmail.com>
2019-07-31 17:33:49 +0100

    font-end deployed to netlify: "npm run build && netlify deploy --dir=build --prod" to update

---
+++ build/precache-manifest.4a032d27d220490b0162810a09b8a3cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31ad06185bb0fe6e3ba854692877aeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdfac52016aa0e7e5c33",
+    "url": "/static/css/main.5149e88f.chunk.css"
+  },
+  {
+    "revision": "a084fc68ef86947013e7",
+    "url": "/static/js/2.166edf9b.chunk.js"
+  },
+  {
+    "revision": "cdfac52016aa0e7e5c33",
+    "url": "/static/js/main.99423820.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
